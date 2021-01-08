54ef84db6d3942e99c503bde27d9821d16c47717
Javier Aransay <aransayj@gmail.com>
2021-01-08 09:33:34 +0100

    recover sum

--- src/test/java/com/sjaluser/calculator/CalculatorApplicationTests.java
+++ src/test/java/com/sjaluser/calculator/CalculatorApplicationTests.java
@@ -16,12 +16,12 @@ class CalculatorTests {
 	
 	@Test
 	 public void testSum() {
-		assertEquals(10, calculator.sum(2, 3));
+		assertEquals(5, calculator.sum(2, 3));
 	 }
 	
 	@Test
 	 public void testSum2() {
-		assertEquals(8, calculator.sum(2, 2));
+		assertEquals(4, calculator.sum(2, 2));
 	 }
 
 }
