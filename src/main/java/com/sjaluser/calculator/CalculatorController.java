6010902a403a86d199521751230dbd0bb117cf95
Javier Aransay <aransayj@gmail.com>
2021-02-15 19:25:36 +0100

    cors

--- src/main/java/com/sjaluser/calculator/CalculatorController.java
+++ src/main/java/com/sjaluser/calculator/CalculatorController.java
@@ -4,10 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 @RestController
 class CalculatorController {
  @Autowired
  private Calculator calculator;
+ 	 @CrossOrigin
 	 @RequestMapping("/sum")
 	 public String sum(@RequestParam("a") Integer a,
 	 @RequestParam("b") Integer b) {
